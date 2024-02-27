2564b2b22acc6b231ef9192a27d17791b67586ce
bvercaem <bvercaem@student.s19.be>
2024-02-27 18:09:49 +0100

    added ex00/megaphone, not sure if it is in a c++ manner tho..

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+static void	str_to_upper(char *str)
+{
+	while (*str)
+	{
+		*str = toupper(*str);
+		str++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+		return 0;
+	}
+	int i = 0;
+	while (++i < ac)
+	{
+		str_to_upper(av[i]);
+		std::cout<<av[i];
+	}
+	std::cout<<std::endl;
+}
