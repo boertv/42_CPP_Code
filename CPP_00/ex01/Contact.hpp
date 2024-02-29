92c20ecdd012f686671d3caa10e6d589d802c719
bvercaem <bvercaem@student.s19.be>
2024-02-29 19:20:43 +0100

    first push of ex01

---
+++ CPP_00/ex01/Contact.hpp
@@ -0,0 +1,16 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class Contact
+{
+	int		index;
+	string	first_name;
+	string	last_name;
+	string	nickname;
+	int		nbr;
+	string	darkest_secret;
+};
+
+#endif
