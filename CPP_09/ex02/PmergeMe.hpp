c06786ffbdd8c45708c6da257a802c0c34a14765
bvercaem <bvercaem@student.s19.be>
2024-05-03 18:21:28 +0200

    negligible

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -1,4 +1,15 @@
 #ifndef PMERGEME_H
 # define PMERGEME_H
 
+# include <vector>
+# include <list>
+
+class LetMeSortThatOutForYou
+{
+	std::vector<int> vcr;
+	std::list<int> lst;
+
+public:
+};
+
 #endif
